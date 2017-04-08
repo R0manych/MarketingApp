9000c054501675b43a5497e7e0a686660956ae56
Roman Chernigovskikh <rchernigovskix@rambler.ru>
2017-04-08 22:51:02 +0300

    Accept form commit
    Added accept form

---
+++ Desktop/Forms/Common/AcceptForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Desktop.Forms.Common
+{
+    partial class AcceptForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вы действительно хотите удалить запись? ";
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonYes.Location = new System.Drawing.Point(16, 50);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(121, 34);
+            this.buttonYes.TabIndex = 1;
+            this.buttonYes.Text = "Да";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonNo.Location = new System.Drawing.Point(231, 50);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(121, 34);
+            this.buttonNo.TabIndex = 2;
+            this.buttonNo.Text = "Нет";
+            this.buttonNo.UseVisualStyleBackColor = true;
+            this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
+            // 
+            // AcceptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 96);
+            this.Controls.Add(this.buttonNo);
+            this.Controls.Add(this.buttonYes);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(382, 135);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(382, 135);
+            this.Name = "AcceptForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Удалить";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonYes;
+        private System.Windows.Forms.Button buttonNo;
+    }
+}
---
+++ Desktop/Forms/Common/AcceptForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop.Forms.Common
+{
+    public partial class AcceptForm : Form
+    {
+        public AcceptForm()
+        {
+            InitializeComponent();
+            Accepted = false;
+        }
+
+        public bool Accepted;
+
+        public AcceptForm(string textMessage)
+        {
+            InitializeComponent();
+            Accepted = false;
+            label1.Text = textMessage;      
+        }
+
+        private void buttonNo_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonYes_Click(object sender, EventArgs e)
+        {
+            Accepted = true;
+            Close();
+        }
+    }
+}
