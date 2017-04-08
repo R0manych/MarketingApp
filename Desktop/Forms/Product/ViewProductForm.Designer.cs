2c17f4cc1ec31aa342f300a36017527cdbf1ef1a
Roman Chernigovskikh <rchernigovskix@rambler.ru>
2017-04-09 00:32:55 +0300

    ViewProductForm changes commt
    Edited size

--- Desktop/Forms/Product/ViewProductForm.Designer.cs
+++ Desktop/Forms/Product/ViewProductForm.Designer.cs
@@ -123,6 +123,8 @@
             this.Controls.Add(this.dataGrid);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(795, 476);
+            this.MinimumSize = new System.Drawing.Size(795, 476);
             this.Name = "ViewProductForm";
             this.Text = "Товар";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ViewProductForm_FormClosing);
