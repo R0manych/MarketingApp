9b308fd5845f179925201fe79473f0983054c559
Roman Chernigovskikh <rchernigovskix@rambler.ru>
2017-04-08 22:53:47 +0300

    View Product form commit
    Added ViewProductForm

--- Desktop/Forms/Product/ViewProductForm.Designer.cs
+++ Desktop/Forms/Product/ViewProductForm.Designer.cs
@@ -29,10 +29,121 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.marketingAppDataDataSet = new Desktop.MarketingAppDataDataSet();
+            this.productsTableAdapter = new Desktop.MarketingAppDataDataSetTableAdapters.ProductsTableAdapter();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marketingAppDataDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AutoGenerateColumns = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn});
+            this.dataGrid.DataSource = this.productsBindingSource;
+            this.dataGrid.Location = new System.Drawing.Point(18, 18);
+            this.dataGrid.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(746, 369);
+            this.dataGrid.TabIndex = 0;
+            this.dataGrid.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGrid_CellBeginEdit);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Наименование";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 300;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Описание";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.Width = 300;
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Цена";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.marketingAppDataDataSet;
+            // 
+            // marketingAppDataDataSet
+            // 
+            this.marketingAppDataDataSet.DataSetName = "MarketingAppDataDataSet";
+            this.marketingAppDataDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(18, 395);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(133, 35);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(632, 395);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(133, 35);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "Закрыть";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // ViewProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ViewProductForm";
+            this.ClientSize = new System.Drawing.Size(777, 437);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.dataGrid);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ViewProductForm";
+            this.Text = "Товар";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ViewProductForm_FormClosing);
+            this.Load += new System.EventHandler(this.ViewProductForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marketingAppDataDataSet)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private MarketingAppDataDataSet marketingAppDataDataSet;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private MarketingAppDataDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonClose;
     }
 }
--- Desktop/Forms/Product/ViewProductForm.cs
+++ Desktop/Forms/Product/ViewProductForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Desktop.Forms.Common;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,50 @@ namespace Desktop.Forms.Product
         public ViewProductForm()
         {
             InitializeComponent();
+        }
+
+        private bool _isModified;
+
+        private void ViewProductForm_Load(object sender, EventArgs e)
+        {
+            _isModified = false;
+            productsTableAdapter.Fill(marketingAppDataDataSet.Products);
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                productsBindingSource.EndEdit();
+                productsTableAdapter.Update(marketingAppDataDataSet.Products);
+                MessageBox.Show("Данные обновлены");
+                _isModified = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка записи {ex.Message}");
+            }
+        }
+
+        private void ViewProductForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_isModified)
+            {
+                var acceptForm = new AcceptForm("Закрыть без сохранения изменений?");
+                acceptForm.ShowDialog();
+                e.Cancel = !acceptForm.Accepted;
+            }
+        }
+
+        private void dataGrid_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            if (!_isModified)
+                _isModified = true;
         }
     }
 }
